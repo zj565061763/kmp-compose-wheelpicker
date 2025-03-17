a2c292a64709ccd486605877885f7699c243a146
zjun <565061763@qq.com>
2025-03-17 16:40:28 +0800

    Sample

--- composeApp/src/commonMain/kotlin/com/sd/demo/kmp/compose_wheelpicker/SampleDefault.kt
+++ composeApp/src/commonMain/kotlin/com/sd/demo/kmp/compose_wheelpicker/SampleDefault.kt
@@ -1,10 +1,12 @@
 package com.sd.demo.kmp.compose_wheelpicker
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.sd.lib.kmp.compose_wheelpicker.FHorizontalWheelPicker
 import com.sd.lib.kmp.compose_wheelpicker.FVerticalWheelPicker
 
 @Composable
@@ -17,6 +19,13 @@ fun SampleDefault(
   ) {
     FVerticalWheelPicker(
       modifier = Modifier.width(128.dp),
+      count = 50,
+    ) { index ->
+      Text(index.toString())
+    }
+
+    FHorizontalWheelPicker(
+      modifier = Modifier.height(128.dp),
       count = 50,
     ) { index ->
       Text(index.toString())
